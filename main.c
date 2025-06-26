4767a7e8b299ae5d2649efb9f757d39c1cab1490
Rafael Araújo <rafaelaraujosouza3333@gmail.com>
2025-06-26 15:57:54 -0300

    Escrita

--- main.c
+++ main.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
 #include "aluno.h"
 #include "curso.h"
 #include "ordenacaoAluno.h"
 #include "buscaSequencialAluno.h"
 #include "buscaBinariaAluno.h"
 
+//Feito por Rafael Araújo e Íthan Amaral
+
 void menu() {
-    printf("\n==== MENU ====\n");
-    printf("1. Buscar por matrícula (sequencial)\n");
-    printf("2. Buscar por nome\n");
-    printf("3. Buscar por curso\n");
-    printf("4. Buscar por matrícula (binária)\n");
+    printf("\n----------- MENU UFOP ------------\n");
+    printf("1. Buscar matricula (busca sequencial)\n");
+    printf("2. Buscar aluno por nome\n");
+    printf("3. Buscar aluno por curso\n");
+    printf("4. Buscar matricula (busca binaria)\n");
     printf("5. Sair\n");
-    printf("Escolha uma opção: ");
+    printf("Escolha uma opcao: ");
 }
 
 int main() {
+
+    setlocale(LC_ALL, "Portuguese");
+
     FILE *arq, *log, *arquivo_cursos;
 
     if ((log = fopen("log_aluno.txt", "a+")) == NULL) { perror("log"); exit(1); }
     if ((arq = fopen("aluno.dat", "w+b")) == NULL) { perror("aluno.dat"); exit(1); }
     if ((arquivo_cursos = fopen("curso.dat", "w+b")) == NULL) { perror("curso.dat"); exit(1); }
 
-    // Inicializar dados
+    // inicializando dados
     criarBaseCursos(arquivo_cursos, 5);
     criarBaseAlunos(arq, 1000);
     ordenaMergeAlunos(arq);
@@ -37,24 +43,25 @@ int main() {
 
         if (opcao == 1) {
             int mat;
-            printf("Digite a matrícula a buscar (sequencial): ");
+            printf("Digite a matricula que voce deseja buscar (sequencial): ");
             scanf("%d", &mat);
             TAluno *a = buscaSequencialAluno(mat, arq, log);
             if (a) {
                 imprime_aluno(a);
                 free(a);
             } else {
-                printf("Aluno não encontrado.\n");
+                printf("Aluno nao encontrado na base de dados!\n");
             }
 
         } else if (opcao == 2) {
             char nomeBusca[50];
-            printf("Digite parte do nome do aluno: ");
+            printf("Digite o nome do aluno: ");
             fgets(nomeBusca, sizeof(nomeBusca), stdin);
             nomeBusca[strcspn(nomeBusca, "\n")] = 0; // remove \n
             rewind(arq);
             TAluno *a;
             int encontrou = 0;
+
             while ((a = le_aluno(arq)) != NULL) {
                 if (strstr(a->nome, nomeBusca)) {
                     imprime_aluno(a);
@@ -62,7 +69,7 @@ int main() {
                 }
                 free(a);
             }
-            if (!encontrou) printf("Nenhum aluno com esse nome.\n");
+            if (!encontrou) printf("Não há nenhum aluno com esse nome.\n");
 
         } else if (opcao == 3) {
             int codigoCurso;
@@ -94,7 +101,7 @@ int main() {
             }
 
         } else if (opcao != 5) {
-            printf("Opção inválida!\n");
+            printf("Opcao invalida!\n");
         }
 
         rewind(arq);
